77bbb8f3ec5e8f67da64bff3f24b7d07a1323a85
red-patriot <realredpatriot@gmail.com>
2019-10-21 16:17:31 -0600

    Decrease Letter size.
    Made Letter a struct as opposed to a class. Add Letter functionality to matrix class. Removed bloat from all classes

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-#include <SDL2/SDL.h>
 #include <iostream>
 
 #include "matrix.h"
-#include "letter.h"
 
-int main(int, char**) {
-  std::cout << sizeof(Letter) << '\n'
-            << sizeof(char) << '\n';
+int main() {
   Matrix matrix = Matrix();
-  
-  // attempt to initialize SDL
-  if (!matrix.init()) {
-    std::cout << "Initialization failed.\n";
+  if(!matrix.init()) {
+    std::cout << "Failure.\n";
     return -1;
   }
 
   matrix.run_loop();
-
+  
   return 0;
 }
 
 /*
- * TODO: Make letters go black faster;
- * TODO: figure out some way for the Matrix::letters stay a constant size
+ * TODO: 
  */
